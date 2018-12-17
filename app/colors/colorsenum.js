1d558bf680ed897cd9be564be58db488c2099824
Arno Dekkers Los <A_Dekkerslos@hotmail.com>
2018-12-17 12:05:35 +0100

    updated the files for the new file structure

---
+++ app/colors/colorsenum.js
@@ -0,0 +1,17 @@
+// use it as module
+var Enum = require('enum');
+ 
+// or extend node.js with this new type
+require('enum').register();
+ 
+//define enum type without flag
+var myEnum = new Enum({'Yellow': 0, 'Red':1, 'Blue': 2, 'Green': 3, 'Purple': 4, 'Orange': 5});
+myEnum;
+myEnum.isFlaggable; //=> false
+ 
+myEnum.toJSON();
+JSON.stringify(myEnum);
+
+module.exports = {
+    get: (x) => myEnum.get(x),
+}
