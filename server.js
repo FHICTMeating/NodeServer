2e5ff53658f52e40ef5512a09fd02e673b56b0ef
bras1223 <l.hermans98@gmail.com>
2018-12-17 12:35:59 +0100

    new port

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const mongoose       = require('mongoose');
 const bodyParser     = require('body-parser');
 const app            = express();
 
-const port = 8000;
 
 mongoose.connect('mongodb://meating:maatwerk1@ds135844.mlab.com:35844/ic');
 const db = mongoose.connection;
+
+var port = process.env.PORT || 3000;
 
 require('./app/routes')(app);
 app.listen(port, () => {
