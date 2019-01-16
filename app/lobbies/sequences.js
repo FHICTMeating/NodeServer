9481854bbd434b0ebbcf577ae83d23cf5597fc68
Arno Dekkers Los <A_Dekkerslos@hotmail.com>
2019-01-16 13:13:08 +0100

    final settings for the event

--- app/lobbies/sequences.js
+++ app/lobbies/sequences.js
@@ -1,5 +1,5 @@
 var sequences = ["The current minors of Fontys are broad enough and changes are not needed", 
-"ICT-related studies should have gymnastics included in the course", 
+"IT-related studies should have gymnastics included in the course", 
 "Partners in Education should be more involved in projects of students",
 "Further automation and robotisation will improve the lives of ordinary Dutch people",
 "The government must realize a database with DNA profiles of all citizens",
